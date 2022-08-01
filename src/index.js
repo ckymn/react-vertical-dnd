abe3d31f06ab63f296a37568266271b175f57b12
ckymn <cokyamanmuhammet@gmail.com>
2022-08-01 17:29:15 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,97 +1,53 @@
-import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import styled from "@emotion/styled";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-// import type { Quote as QuoteType } from "../types";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 
-const initial = Array.from({ length: 10 }, (v, k) => k).map((k) => {
-  const custom = {
-    id: `id-${k}`,
-    content: `Quote ${k}`,
+function App() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [height, setHeight] = useState(0);
+  const rect = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll, true);
+    return () => window.removeEventListener("scroll", listenToScroll, true);
+  }, []);
+
+  const listenToScroll = () => {
+    if (!rect.current) return;
+    const { top, bottom } = rect.current.getBoundingClientRect();
+    // let heightToHideFrom = 200; // eger burda sabit deger verirsek calisiyor.
+    // TODO: burda map yaparsak isVisible nasil kullanicaz ?
+    // butun data'lara ayri ref() mi acmak gerek?
+
+    setHeight(bottom);
+    if (bottom > 0) {
+      console.log("true");
+      setIsVisible(true);
+    } else if (bottom < 0) {
+      console.log("false");
+      setIsVisible(false);
+      console.log(bottom);
+    }
   };
 
-  return custom;
-});
-
-const grid = 8;
-const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-
-  return result;
-};
-
-const QuoteItem = styled.div`
-  width: 200px;
-  border: 1px solid grey;
-  margin-bottom: ${grid}px;
-  background-color: lightblue;
-  padding: ${grid}px;
-`;
-
-function Quote({ quote, index }) {
   return (
-    <Draggable draggableId={quote.id} index={index}>
-      {(provided) => (
-        <QuoteItem
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          {quote.content}
-        </QuoteItem>
-      )}
-    </Draggable>
-  );
-}
-
-const QuoteList = React.memo(function QuoteList({ quotes }) {
-  return quotes.map((quote, index) => (
-    <Quote quote={quote} index={index} key={quote.id} />
-  ));
-});
-
-function QuoteApp() {
-  const [state, setState] = useState({ quotes: initial });
-
-  function onDragEnd(result) {
-    if (!result.destination) {
-      return;
-    }
-
-    if (result.destination.index === result.source.index) {
-      return;
-    }
-
-    const quotes = reorder(
-      state.quotes,
-      result.source.index,
-      result.destination.index
-    );
-
-    setState({ quotes });
-  }
-
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="list">
-        {(provided, snapshot) => (
-          <div
-            ref={provided.innerRef}
-            style={{
-              backgroundColor: snapshot.isDraggingOver ? "blue" : "grey",
-            }}
-            {...provided.droppableProps}
-          >
-            <QuoteList quotes={state.quotes} />
-            {provided.placeholder}
+    <div id="container">
+      <div id="height">
+        <b>
+          height: {height} - {isVisible ? "show" : "hide"}
+        </b>
+      </div>
+      {isVisible && (
+        <>
+          <div id="hide" ref={rect}>
+            Top div {isVisible && "| in viewport"}
           </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+          <div id="hide" ref={rect}>
+            Top div {isVisible && "| in viewport"}
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<QuoteApp />, rootElement);
+ReactDOM.render(<App />, document.getElementById("root"));
